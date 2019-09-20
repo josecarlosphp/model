9f55677113bd36d8b712ba584510140cce9989ce
josecarlosphp <38591375+josecarlosphp@users.noreply.github.com>
2019-09-20 10:42:08 +0200

    Mejorando el switch de $class.

--- src/Node.php
+++ src/Node.php
@@ -99,7 +99,7 @@ class Node
             {
                 if(array_key_exists($key, $array))
                 {
-                    $class = basename(get_class($child));
+                    $class = basename(str_replace('\\', '/', get_class($child)));
                     switch($class)
                     {
                         case 'Node':
